a0953ae3cb1b417b06e8e0f2635d012b4bf8d81c
Loic Ovigne <ovigne1@univ-lorraine.fr>
2024-11-27 15:37:47 +0100

    add coaster

---
+++ src/Controller/CoasterController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Coaster;
+use App\Form\CoasterType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class CoasterController extends AbstractController
+{
+    #[Route(path: '/coaster/add')]
+    public function add(EntityManagerInterface $em, Request $request): Response
+    {
+        $coaster = new Coaster();
+        /*$coaster->setName('Blue Fire')
+            ->setMaxHeight(38)
+            ->setMaxSpeed(100)
+            ->setLength(1056)
+            ->setOperating(true)
+        ;*/
+
+        $form = $this->createForm(CoasterType::class, $coaster);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // ajoute la nouvelle entité dans le manager Doctrine
+            $em->persist($coaster);
+
+            // Met à jour la DB
+            $em->flush();
+
+            return $this->redirectToRoute('app_app_index');
+        }
+
+        return $this->render('coaster/add.html.twig', [
+            'coasterForm' => $form,
+        ]);
+    }
+}
